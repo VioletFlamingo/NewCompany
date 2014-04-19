968757301b74dc79d8bd840bb75712598a4b6a3a
Paulina <paulina.winkowska@gmail.com>
2014-04-20 00:59:10 +0200

    Initial commit

---
+++ src/lab05/DoAllWork.java
@@ -0,0 +1,10 @@
+package lab05;
+
+/**
+ * Created by Paulina on 19.04.2014.
+ */
+public class DoAllWork implements WorkStrategy {
+    public int doWork(int work) {
+        return work;
+    }
+}
---
+++ src/lab05/WorkStrategy.java
@@ -0,0 +1,8 @@
+package lab05;
+
+/**
+ * Created by Paulina on 19.04.2014.
+ */
+public interface WorkStrategy {
+    int doWork(int work);
+}
